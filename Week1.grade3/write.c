4e3cc8a30c285ca82e7236b846018e9208bfc6f9
Laskin00 <ale.laskintues19@gmail.com>
2017-12-03 22:02:32 +0200

    Did my best for the time given

---
+++ Week1.grade3/write.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int fd, State;
+    char buff[500];
+
+
+    mkfifo("tmp", 0666);
+
+    if((fd = open("tmp", O_WRONLY)) < 0){
+      printf("Cannot open\n");
+      return 0;
+    }
+
+
+    while((State = read(STDIN_FILENO, buff, 500) ) > 0) {
+      write(fd, buff, strlen(buff));
+    }
+    close(fd);
+}
