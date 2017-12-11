e72b49758e6c4ed185f0ccf4863f8eefc862b866
Laskin00 <ale.laskintues19@gmail.com>
2017-12-11 20:37:07 +0200

    Update write.c

--- Week1.grade3/write.c
+++ Week1.grade3/write.c
@@ -34,51 +34,53 @@ int main(){
 #include <string.h>
 #include <stdlib.h>
 
- struct ringbuff{
- 		int pos;
- 		int read;
+struct ringbuff{
+    int pos;
+    int read;
     void *head;
     void *tail;
- 		char begin[0];
- };
+    char begin[0];
+};
 
- int main(int argc, char * argv[]){
-
- 	int mempos = shm_open( "tmp", O_CREAT | O_RDWR, S_IRWXO );
-
- 	if( mempos == -1 )
- 	{
- 		perror("Can't open file");
+int main(int argc, char * argv[]){
+    int mempos = shm_open( "tmp", O_CREAT | O_RDWR, S_IRWXO );
+    
+    if( mempos == -1 ){
+        perror("Can't open file");
  		return 0;
  	}
 
  	int res;
+    
  	res = ftruncate( mempos, 4096 );
+    
  	if( res == -1 ){
  		perror("truncating");
  		return 0;
  	}
 
  	struct ringbuff* rb = mmap( NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, mempos, 0 );
- 	if( rb == NULL )
- 	{
+ 	if( rb == NULL ){
  		perror("mmapping");
  		return 0;
  	}
 
- 	rb->pos = 0;
+ rb->pos = 0;
   rb->read = 0;
   rb->head = rb->begin;
   rb->tail= rb->begin;
 
- 	char buff [512];
-  int i = 0;
+    char buff [512];
+    int i = 0;
+    
  	while(1){
  		fgets(buff, 1, stdin);
-    i++;
-    if(i == 511){
-      system("./read");
-    }
+        i++;
+        
+        if(i == 511){
+             system("./read");
+         }
+        
  		memcpy( rb->begin + rb->pos, buff, strlen(buff) + 1);
  		rb->pos += strlen(buff);
  	}
